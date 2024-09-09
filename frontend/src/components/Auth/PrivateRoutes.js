48f25d3e074b557d386b48b8a544d0110ac35cf4
discussion forall <63451373+discussionforall@users.noreply.github.com>
2024-09-09 12:34:49 +0530

    Added Readme

--- frontend/src/components/Auth/PrivateRoutes.js
+++ frontend/src/components/Auth/PrivateRoutes.js
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ role, children }) => {
     const userRole = localStorage.getItem('userRole'); // You can store this after successful login
